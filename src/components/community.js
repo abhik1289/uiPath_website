57d370cb15958b947f97f9bbb5850dc02c2e5631
Abhik Patra <116542605+abhik1289@users.noreply.github.com>
2024-03-29 22:21:55 +0530

    Update community.js changes

--- src/components/community.js
+++ src/components/community.js
@@ -2,6 +2,6 @@ import React from 'react'
 
 export default function Community() {
     return (
-        <section style={{background:"#2ecc71"}}>community page here</section>
+        <section style={{background:"#2ecc71"}}>community page </section>
     )
 }
